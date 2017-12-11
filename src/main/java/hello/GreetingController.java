1dbf84ef4b994fda5a4a642c29821358e6d69890
Pusinfo-081 <Pusinfo-081@Pusinfowas-081>
2017-12-11 14:18:06 +0700

    Belum bersih (masih harus diperbaiki)

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -2,11 +2,17 @@ package hello;
 
 import static org.assertj.core.api.Assertions.filter;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
+import org.omg.CORBA_2_3.portable.InputStream;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,9 +47,28 @@ public class GreetingController {
     	//return data;
     }
     	
-    
+    @RequestMapping("/countries")
+    public String getCountries() throws MalformedURLException{
+    	URL url = new URL("http://www.webservicex.net/country.asmx/GetCountries?");
+    	
+    	URLConnection connectiion = url.openConnection();
+    	connection.setDoOutput (true);
+    	
+    	InputStream stream = url.openConnection().getInputStream();
+    	InputStreamReader reader = new InputStreamReader(stream);
+    	BufferedReader buffer = new BufferedReader(reader);
+    	
+    	String line;
+    	StringBuilder builder = new StringBuilder();
+    	while((line = buffer.readLine())!= null) {
+    		builder.append(line);
+    	}
+    	
+    	
+    	
+    }
     
     
 
-    
+     
 }
